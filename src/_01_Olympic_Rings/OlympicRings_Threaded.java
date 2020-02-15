06c268813160337e5d269cccf4a38a921e12c83b
league <league@imac-24.attlocal.net>
2020-02-15 11:27:35 -0800

    Finished olympic rings and robot race

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,79 @@
 package _01_Olympic_Rings;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	static Robot r1 = new Robot(200, 300);
+	static Robot r2 = new Robot(300, 300);
+	static Robot r3 = new Robot(400, 300);
+	static Robot r4 = new Robot(250, 350);
+	static Robot r5 = new Robot(350, 350);
+	static Robot[] robots = new Robot[5];
+	
+
+
+	public static void main(String[] args) {
+r1.setPenColor(Color.blue);
+r2.setPenColor(Color.black);
+r3.setPenColor(Color.red);
+r4.setPenColor(Color.green);
+r5.setPenColor(Color.yellow);
+
+		robots[0] = r1;
+		robots[1] = r2;
+		robots[2] = r3;
+		robots[3] = r4;
+		robots[4] = r5;
+		for (Robot r : robots) {
+			r.penDown();
+			r.hide();
+		}
+		Thread ring1 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r1.move(1);
+				r1.turn(1);
+			}
+
+		});
+		Thread ring2 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r2.move(1);
+				r2.turn(1);
+			}
+
+		});
+		Thread ring3 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r3.move(1);
+				r3.turn(1);
+			}
+
+		});
+		Thread ring4 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r4.move(1);
+				r4.turn(1);
+			}
+
+		});
+		Thread ring5 = new Thread(() -> {
+			for (int i = 0; i < 360; i++) {
+				r5.move(1);
+				r5.turn(1);
+			}
+
+		});
+
+
+		ring1.start();
+		ring2.start();
+		ring3.start();
+		ring4.start();
+		ring5.start();
+
+
+	}
 
 }
-
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,51 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JFrame;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+	ArrayList<Robot> racers = new ArrayList<Robot>();
+	ArrayList<Thread> threads = new ArrayList<Thread>();
+	int speed;
+
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		AdvancedRobotRace robRace = new AdvancedRobotRace();
+		robRace.setup();
+		System.out.println(robRace.threads.size());
+		for (Thread t : robRace.threads) {
+			t.start();
+		}
+
+	}
+
+	protected void setup() {
+		JFrame f = new JFrame();
+		f.setSize(800, 300);
+		f.setResizable(false);
+		Random rand = new Random();
+		for (int i = 0; i < 23; i++) {
+			racers.add(new Robot(50 + (i * 75), 800));
+		}
+		for (Robot r : racers) {
+			speed = rand.nextInt(10);
+			do {
+				speed = rand.nextInt(10);
+			} while (!(speed > 1));
+			r.setSpeed(speed);
+
+		}
+		for (int i = 0; i < 23; i++) {
+			Robot current = racers.get(i);
+			threads.add(new Thread(() -> {
+				current.move(800);
+			}));
+		}
+
+	}
 }
